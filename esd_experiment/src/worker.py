a895dc3a5acb2ee19d717fa232432a880a2d8c11
RpM-Kinshuk <103813028+RpM-Kinshuk@users.noreply.github.com>
2025-10-21 15:15:43 -0400

    Repurpose fast estimator to net esd estimator

--- esd_experiment/src/worker.py
+++ esd_experiment/src/worker.py
@@ -26,7 +26,7 @@ sys.path.insert(0, str(PROJECT_ROOT))
 sys.path.insert(0, str(SCRIPT_DIR))
 
 from model_loader import load_model, parse_model_string, safe_filename
-from esd_estimator.fast_esd_estimator import net_esd_estimator
+from esd_estimator.net_esd_estimator import net_esd_estimator
 
 
 def parse_args():
