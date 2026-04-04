5736a561f7fa245abd477ba654d0ce3eb82622b8
RpM-Kinshuk <103813028+RpM-Kinshuk@users.noreply.github.com>
2026-04-04 06:48:18 -0400

    Remove redundant code

--- cmp.sh
+++
@@ -1,5 +0,0 @@
-python scatter.py \
-  --dir_a /jumbo/yaoqingyang/kinshuk/Model-Zoo/svd_results \
-  --dir_b /jumbo/yaoqingyang/kinshuk/Model-Zoo/atlas_results \
-  --metric alpha \
-  --interactive
